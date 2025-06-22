dfd3203b794386a837da03e054aad0826b03e73e
Karim Feruzy <167116240+FeruzyNtillah@users.noreply.github.com>
2025-06-22 22:59:39 +0300

    Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,39 +1,78 @@
 import React from 'react';
-import { List, ListItem, ListItemIcon, ListItemText, Divider } from '@mui/material';
-import { Home, Event, LibraryBooks, AttachMoney, People } from '@mui/icons-material';
+import { 
+  List, 
+  ListItem, 
+  ListItemIcon, 
+  ListItemText, 
+  Divider,
+  Box,
+  useTheme
+} from '@mui/material';
+import { 
+  Home, 
+  Event, 
+  LibraryBooks, 
+  AttachMoney, 
+  People 
+} from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
+  const theme = useTheme();
+  
   return (
-    <div style={{ width: 250 }}>
-      <List>
-        <ListItem button component={Link} to="/">
-          <ListItemIcon><Home /></ListItemIcon>
-          <ListItemText primary="Home" />
-        </ListItem>
-        <ListItem button component={Link} to="/events">
-          <ListItemIcon><Event /></ListItemIcon>
-          <ListItemText primary="Events" />
-        </ListItem>
-        <ListItem button component={Link} to="/sermons">
-          <ListItemIcon><LibraryBooks /></ListItemIcon>
-          <ListItemText primary="Sermons" />
-        </ListItem>
-        <ListItem button component={Link} to="/donations">
-          <ListItemIcon><AttachMoney /></ListItemIcon>
-          <ListItemText primary="Donations" />
-        </ListItem>
-        <ListItem button component={Link} to="/Addmembers">
-          <ListItemIcon><People /></ListItemIcon>
-          <ListItemText primary="Add members" />
-        </ListItem>
-        <ListItem button component={Link} to="/members">
-          <ListItemIcon><People /></ListItemIcon>
-          <ListItemText primary="Members" />
-        </ListItem>
-      </List>
+    <Box 
+      sx={{
+        width: 250,
+        height: '100vh',
+        borderRight: `1px solid ${theme.palette.divider}`,
+        backgroundColor: theme.palette.background.paper,
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+    >
+      <Box sx={{ flexGrow: 1 }}>
+        <List sx={{ py: 0 }}>
+          {[
+            { icon: <Home />, text: "Home", path: "/" },
+            { icon: <Event />, text: "Events", path: "/events" },
+            { icon: <LibraryBooks />, text: "Sermons", path: "/sermons" },
+            { icon: <AttachMoney />, text: "Donations", path: "/donations" },
+            { icon: <People />, text: "Add members", path: "/add-members" },
+            { icon: <People />, text: "Members", path: "/members" },
+          ].map((item, index) => (
+            <ListItem 
+              button 
+              component={Link} 
+              to={item.path}
+              key={index}
+              sx={{
+                py: 1.5,
+                px: 3,
+                '&:hover': {
+                  backgroundColor: theme.palette.action.hover,
+                },
+                '&.Mui-selected': {
+                  backgroundColor: theme.palette.action.selected,
+                }
+              }}
+            >
+              <ListItemIcon sx={{ minWidth: 40 }}>
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText 
+                primary={item.text} 
+                primaryTypographyProps={{
+                  variant: 'body1',
+                  fontWeight: 'medium'
+                }}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
       <Divider />
-    </div>
+    </Box>
   );
 };
 
