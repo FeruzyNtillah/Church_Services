60393cd21e56db6fd0a9c682b02f399be8816692
Karim Feruzy <167116240+FeruzyNtillah@users.noreply.github.com>
2025-06-22 22:42:03 +0300

    Blue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, useTheme } from '@mui/material';
+import { tokens } from '../theme';
 
 const Header = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
-    <AppBar position="static" sx={{ mb: 4 }}>
+    <AppBar 
+      position="static" 
+      sx={{ 
+        mb: 4,
+        backgroundColor: colors.primary[700] // Set AppBar background to primary 700
+      }}
+    >
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography 
+          variant="h6" 
+          component="div" 
+          sx={{ 
+            flexGrow: 1,
+            color: colors.grey[100] // Set text color to white
+          }}
+        >
           Church Dashboard
         </Typography>
       </Toolbar>
